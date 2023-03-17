45a59dfe4ddf944aefebc9d227cd8c7f8ac65cbc
Olusoji <esohjay3@gmail.com>
2023-03-17 10:47:11 +0000

    minor fix

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -75,7 +75,7 @@ function Modal({
             <div className="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
               <button
                 type="button"
-                className="inline-block rounded bg-primary-100 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+                className="inline-block rounded bg-primary-100 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-mainColor transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
                 data-te-modal-dismiss
                 data-te-ripple-init
                 data-te-ripple-color="light"
@@ -85,7 +85,7 @@ function Modal({
               <button
                 onClick={action}
                 type="button"
-                className="ml-1 inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                className="ml-1 inline-block rounded bg-mainColor px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-mainColor transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(51, 75, 17,0.3),0_4px_18px_0_rgba(51, 75, 17,0.2)] focus:bg-altColor focus:text-white focus:shadow-[0_8px_9px_-4px_rgba(51, 75, 17,0.3),0_4px_18px_0_rgba(51, 75, 17,0.2)] focus:outline-none focus:ring-0 active:bg-altColor active:text-white active:shadow-[0_8px_9px_-4prgba(51, 75, 17,0.3)x_,0_4px_18px_0_rgba(51, 75, 17,0.2)]"
                 data-te-ripple-init
                 data-te-modal-dismiss
                 data-te-ripple-color="light"
--- frontend/src/pages/hrm/Profile.jsx
+++ frontend/src/pages/hrm/Profile.jsx
@@ -216,6 +216,51 @@ function Profile() {
           >
             <p>{currentData?.firstName} will be deleted permanently</p>
           </Modal>
+          <Modal
+            style="bg-danger px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#dc4c64] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:bg-danger-600 focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:outline-none focus:ring-0 active:bg-danger-700 active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)]"
+            btnText={`Change Password`}
+            targetId="changePw"
+            modalTitle="Change Password"
+            confirmText="submit"
+            action={() => deletStaff(currentData?.id)}
+            // size="small"
+          >
+            <form>
+              {/* email */}
+              <div className="mb-3">
+                <label
+                  htmlFor="password"
+                  className="capitalize font-medium mb-1 block text-sm"
+                >
+                  password
+                </label>
+                <input
+                  type="text"
+                  {...registerEmail("password", { required: true })}
+                  className="p-2 rounded-md mb-2 block bg-white w-full focus:outline-none border border-slate-300"
+                />
+                {errorsEmail.password && (
+                  <span className="text-red-500">password is required</span>
+                )}
+              </div>
+              <div className="mb-3">
+                <label
+                  htmlFor="email"
+                  className="capitalize font-medium mb-1 block text-sm"
+                >
+                  email
+                </label>
+                <input
+                  type="text"
+                  {...registerEmail("email", { required: true })}
+                  className="p-2 rounded-md mb-2 block bg-white w-full focus:outline-none border border-slate-300"
+                />
+                {errorsEmail.email && (
+                  <span className="text-red-500">email is required</span>
+                )}
+              </div>
+            </form>
+          </Modal>
         </div>
       </article>
     </article>
