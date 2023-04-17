45a30912eef69dd66a2ca9e86f52743c3647c0f3
Olusoji <esohjay3@gmail.com>
2023-04-17 15:37:15 +0100

    change db cred

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,18 @@
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize("volunteer_manager", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
+module.exports = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PW,
+  {
+    host: process.env.HOST,
+    dialect: "mysql",
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
